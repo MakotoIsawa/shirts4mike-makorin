260ccb414ed0170d9f4afa21b995302904edd28c
Makoto Isawa <makoto.isawa@gmail.com>
2013-06-19 23:26:57 +0900

    inital commit

---
+++ receipt.php
@@ -0,0 +1,22 @@
+<?php
+$pageTitle = "Thank you for your order!";
+$section = "none";
+include("inc/header.php"); ?>
+
+	<div class="section page">
+
+		<div class="wrapper">
+
+			<h1>Thank you!</h1>
+
+			<p>お支払いいただき、ありがとうございました。取引が完了しましたので、購入商品の受領書をメールでお送りしました。この取引の詳細は、<a href="http://www.paypal.com/jp">www.paypal.com/jp</a>でアカウントにログインして確認できます。</p>
+
+			<p>Need another shirt already? Visit <a href="shirts.php">Shirts Listing</a> page again.</p>
+		
+		</div>
+    
+    </div>
+
+<?php include("inc/footer.php"); ?>
+
+
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+
+function sum_two_numbers($one,$two) {
+
+    $sum = $one + $two;
+
+    return $sum;
+
+}
+
+echo sum_two_numbers(1,5);
+
+
+?>
